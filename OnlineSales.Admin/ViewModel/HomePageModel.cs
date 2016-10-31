7b115e0113ccac93c24af24655ec11a52835a047
mehmetalikoker <mehmetalikoker@outlook.com>
2016-10-31 20:09:40 +0300

    .

---
+++ OnlineSales.Admin/ViewModel/HomePageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineSales.Admin.ViewModel
+{
+    /// <summary>
+    /// View Model içinde sayfalara özgü class lar bulunacak. 
+    /// Bu class içinde ana sayfaya özgü değişl-kenler bulunacak.
+    /// </summary>
+    public class HomePageModel
+    {
+        public int CategoryCount { get; set; }
+        public int ProductCount { get; set; }
+        public int ProductImageCount { get; set; }
+        public int ProductFeatureCount { get; set; }
+    }
+}
