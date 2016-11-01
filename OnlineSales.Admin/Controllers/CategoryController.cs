7cd697bd46f4c6f0efd6a9ced8811b70a605a73f
mehmetalikoker <mehmetalikoker@outlook.com>
2016-11-01 12:47:14 +0300

    .

---
+++ OnlineSales.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using OnlineSales.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineSales.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var categoryList = _categoryRepository.GetAll().ToList();
+            return View(categoryList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- OnlineSales.Admin/Controllers/HomeController.cs
+++ OnlineSales.Admin/Controllers/HomeController.cs
@@ -10,9 +10,29 @@ namespace OnlineSales.Admin.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductRepository _productRepository;
+        private ICategoryRepository _categoryRepository;
+        private IProductImageRepository _productImageRepository;
+        private IProductFeatureRepository _productFeatureRepository;
+
+        public HomeController(ICategoryRepository categoryRepository, IProductRepository productRepository, IProductImageRepository productImageRepository, IProductFeatureRepository productFeatureRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+            _productImageRepository = productImageRepository;
+            _productFeatureRepository = productFeatureRepository;
+        }
+
         public ActionResult Index()
         {
-            var pagelModel = new HomePageModel();
+            var pagelModel = new HomePageModel
+            {
+                CategoryCount = _categoryRepository.Count(),
+                ProductCount = _productRepository.Count(),
+                ProductImageCount = _productImageRepository.Count(),
+                ProductFeatureCount = _productFeatureRepository.Count()
+            };
+
             return View(pagelModel);
         }
 
